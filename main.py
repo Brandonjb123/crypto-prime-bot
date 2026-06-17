f9dc6559b6a46cb8fe726303d159411459d0c692
Brandonjb123 <bocibatas2016@gmail.com>
2026-06-17 17:15:15 +0700

    Bersihkan main.py, hapus backup/restore, rapikan import

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
-import logging
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 from loguru import logger
 from config import TELEGRAM_BOT_TOKEN
-from bot.handlers import (
-    start_command,
-    price_command,
-    analyze_command,
-    news_command,
-    help_command,
-)
 from db.database import init_db
 from bot.handlers import (
     start_command, price_command, analyze_command,
     news_command, help_command, usage_command,
     addposition_command, removeposition_command,
-    myportfolio_command, backup_command, restore_command,
-    mysignals_command, paperstats_command, handle_callback,
+    myportfolio_command, mysignals_command, paperstats_command,
     setplan_command, upgrade_command,
+    handle_callback,
 )
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 
-
-# Setup loguru ke file dan console
 logger.add("bot.log", rotation="1 day", retention="7 days", level="DEBUG")
 logger.info("Starting bot...")
 
 def main():
-    # Inisialisasi database
     init_db()
     logger.info("Database siap")
-    
+
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start_command))
     app.add_handler(CommandHandler("price", price_command))
